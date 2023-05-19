20529e0c60212b7edff616a44a37d34685c87eb9
Eojtir <eojtir@gmail.com>
2023-05-18 23:05:49 -0500

    integrando front con back

--- 1-intro-flask.py
+++ 1-intro-flask.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app,origins=['http://localhost:5500','http://127.0.0.1:5500'],methods=['GET','POST','PUT','DELETE'])
 productos = [{
     'id': 1,
     'nombre': 'Martillo',
@@ -25,11 +27,17 @@ productos = [{
 @app.route('/productos', methods=['GET', 'POST'])
 def gestionProductos():
     # request > me da toda la informacion de mi cliente
+
     if request.method == 'GET':
+        productos_existentes = []
+        for producto in productos:
+            if producto is None:
+                continue
+            productos_existentes.append(producto)
         return {
 
-            'message': 'los productos son',
-            'content': productos
+            'message': 'los productos existente son',
+            'content': productos_existentes
 
         }
     elif request.method == 'POST':
@@ -44,22 +52,40 @@ def gestionProductos():
         }
 
 
-@app.route('/producto/<int:id>', methods=["GET"])
+@app.route('/producto/<int:id>', methods=["GET", "PUT", "DELETE"])
 def gestionProducto(id):
+    resultado = None
+
+    for producto in productos:
+        if producto is None:
+            continue
+        if producto['id'] == id:
+            resultado = None
+            resultado = producto
+            break
+    if resultado is None:
+        return {
+            'message': 'no se encontro el producto'
+        }
+
     if request.method == 'GET':
-        resultado = None
-        for producto in productos:
-            if producto['id'] == id:
-                resultado = producto
-                break
-        if resultado == None:
-            return {
-                'message': 'no se encontro el producto'
-            }
-        else:
-            return {
-                'message': 'El producto es'
-            }
+
+        return {
+            'message': 'El producto es',
+            'content': resultado
+        }
+    if request.method == 'PUT':
+        data = request.json
+        productos[id - 1] = data
+        productos[id - 1]['id'] = id
+        return {
+            'message': 'se actualizo el  producto'
+        }
+    if request.method == 'DELETE':
+        productos[id - 1] = None
+        return {
+            'message': "Producto eliminado exitosamente",
+        }
 
 
 if __name__ == "__main__":
